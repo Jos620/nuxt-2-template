e6f2ff1d6ad4ecaacc96d4ea091b540cedef1c22
Mateus Ito Silva <mateusito10@gmail.com>
2021-04-15 19:35:50 -0300

    🧩 | Components - added components (media link)

---
+++ components/global/mediaLink.vue
@@ -0,0 +1,83 @@
+<template>
+    <a :href="link" target="_blank" :class="nameLower">
+        <img :src="require(`~/assets/svg/${nameLower}.svg`)" alt="Github" />
+        <p>{{ name }}</p>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'Media Links',
+
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        }
+    },
+
+    computed: {
+        nameLower: function () {
+            return this.name.toLowerCase();
+        }
+    }
+}
+</script>
+
+<style lang="postcss">
+a {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 12px 0 6px;
+    color: white;
+    width: 132px;
+    height: 64px;
+    border-radius: 6px;
+    position: fixed;
+    bottom: 20px;
+    transition: all 100ms ease-in-out 0s;
+
+    @apply shadow-md hover:shadow-xl;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:hover {
+        bottom: 24px;
+    }
+}
+
+.linkedin {
+    left: 20px;
+    @apply bg-linkedin;
+
+    &:hover {
+        left: 24px;
+    }
+}
+
+.codepen {
+    right: 50%;
+    transform: translateX(50%);
+    @apply bg-codepen;
+
+    &:hover {
+        bottom: 24px;
+    }
+}
+
+.github {
+    right: 20px;
+    @apply bg-github;
+
+    &:hover {
+        right: 24px;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,9 @@
         <h1 class="text-9xl font-semibold tracking-wide">Nuxt JS</h1>
 
         <div>
-            <a href="https://github.com/jos620" target="_blank" class="github">
-                <img src="~/assets/svg/github.svg" alt="Github" />
-                <p>Github</p>
-            </a>
-
-            <a href="https://codepen.io/mateus-ito" target="_blank" class="codepen">
-                <img src="~/assets/svg/codepen.svg" alt="Codepen" />
-                <p>Codepen</p>
-            </a>
-
-            <a
-                href="https://www.linkedin.com/in/mateus-eduardo-ito-silva-b728ba196/"
-                target="_blank"
-                class="linkedin"
-            >
-                <img src="~/assets/svg/linkedin.svg" alt="LinkedIn" />
-                <p>LinkedIn</p>
-            </a>
+            <media-link name="LinkedIn" link="https://linkedin.com/in/mateus-eduardo-ito-silva-b728ba196" />
+            <media-link name="CodePen" link="https://codepen.io/mateus-ito" />
+            <media-link name="GitHub" link="https://github.com/jos620" />
         </div>
     </div>
 </template>
@@ -36,57 +21,5 @@ h1 {
     @apply text-transparent bg-clip-text;
     @apply bg-gradient-to-r;
     @apply from-blue-600 via-cyan-600 to-green-600;
-}
-
-a {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 12px 0 6px;
-    color: white;
-    width: 132px;
-    height: 64px;
-    border-radius: 6px;
-    position: fixed;
-    bottom: 20px;
-    transition: all 100ms ease-in-out 0s;
-
-    @apply shadow-md hover:shadow-xl;
-
-    &:focus {
-        outline: none;
-    }
-
-    &:hover {
-        bottom: 24px;
-    }
-}
-
-.linkedin {
-    left: 20px;
-    @apply bg-linkedin;
-
-    &:hover {
-        left: 24px;
-    }
-}
-
-.codepen {
-    right: 50%;
-    transform: translateX(50%);
-    @apply bg-codepen;
-
-    &:hover {
-        bottom: 24px;
-    }
-}
-
-.github {
-    right: 20px;
-    @apply bg-github;
-
-    &:hover {
-        right: 24px;
-    }
 }
 </style>
