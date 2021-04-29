2d5420aa3436276d65ba27d985fd23e49c604c53
Mateus Ito Silva <mateusito10@gmail.com>
2021-04-29 10:49:56 -0300

    🔍 | SEO - added SEO meta tags for Facebook and Twitter

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,29 @@ export default {
     head: {
         title: process.env.npm_package_name || '',
         htmlAttrs: {
-            lang: 'en',
+            lang: 'pt_BR',
         },
         meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
+            { charset      : 'utf-8' },
+            { name         : 'viewport'         , content : 'width=device-width, initial-scale=1' },
+            { hid          : 'description'      , content : '', name    : 'description'           },
+            { 'http-equiv' : 'content-language' , content : 'pt-br'                               },
+            { 'http-equiv' : 'content-type'     , content : 'text/html; charset=UTF-8'            },
+            { name         : 'creator'          , content : 'Mateus Eduardo Ito Silva'            },
+            { name         : 'keywords'         , content : ''                                    },
+
+            // Facebook
+            { property : 'og:locale'      , content : 'pt_BR'   },
+            { property : 'og:type'        , content : 'website' },
+            { property : 'og:title'       , content : ''        },
+            { property : 'og:description' , content : ''        },
+            { property : 'og:url'         , content : ''        },
+            { property : 'og:image'       , content : ''        },
+
+            // Twitter
+            { name : 'twitter:card'        , content : 'summary' },
+            { name : 'twitter:title'       , content : ''        },
+            { name : 'twitter:description' , content : ''        }
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
